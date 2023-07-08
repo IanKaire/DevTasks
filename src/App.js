c0de995d7896a59243e9cb5a090b69c88b9d0277
Ian Kaire <iankaire@gmail.com>
2023-07-08 17:44:22 +0300

    first commit for devtasks project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  Route, 
+  RouterProvider 
+} from 'react-router-dom';
+
+// layouts and pages
+import RootLayout from './layouts/RootLayout';
+import Dashboard from './pages/Dashboard';
+import Create from './pages/Create';
+import Profile from './pages/Profile';
+
+// router and routes
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Dashboard />} />
+      <Route path="create" element={<Create />} />
+      <Route path="profile" element={<Profile />} />
+    </Route>
+  )
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <RouterProvider router={router} />
+  )
 }
 
 export default App;
