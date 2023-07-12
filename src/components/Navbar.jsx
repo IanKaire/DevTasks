ba9ec922aee16d2969abfeecb3de97b230441825
Ian Kaire <iankaire@gmail.com>
2023-07-12 13:32:18 +0300

    project completion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,43 @@
-import { Flex, Heading, Box, Text, Button, Spacer, HStack } from "@chakra-ui/react"
+import { UnlockIcon } from "@chakra-ui/icons"
+import { 
+  Flex, 
+  Heading, 
+  Text, 
+  Button, 
+  Spacer, 
+  HStack, 
+  useToast, 
+  AvatarBadge,
+  Avatar
+} from "@chakra-ui/react"
 
 export default function Navbar() {
+  const toast = useToast()
+
   return (
     <Flex as="nav" p="10px" mb="60px" alignItems="center">
       <Heading as="h1" fontSize="1.5em">Dev Tasks</Heading>
       <Spacer />
 
       <HStack spacing="20px"> 
-        <Box bg="gray.200" p="10px 15px" borderRadius="50%">H</Box>
+        <Avatar name="mario" src="/img/harry.jpg">
+          <AvatarBadge width="1.3em" bg="teal.500">
+            <Text fontSize="xs" color="white">3</Text>
+          </AvatarBadge>
+        </Avatar>
         <Text>harry@gmail.com</Text>
-        <Button colorScheme="purple">Logout</Button>
+        <Button 
+          colorScheme="purple"
+          onClick={() => toast({
+            title: 'Logged out.',
+            description: "Successfully logged out",
+            duration: 10000,
+            isClosable: true,
+            position: 'top',
+            status: 'success',
+            icon: <UnlockIcon />,
+          })}
+        >Logout</Button>
       </HStack>
     </Flex>
   )
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,76 @@
+import { 
+  ChatIcon, 
+  CheckCircleIcon, 
+  EmailIcon, 
+  StarIcon, 
+  WarningIcon 
+} from '@chakra-ui/icons'
+import { 
+  Tabs, 
+  TabList, 
+  Tab, 
+  TabPanels, 
+  TabPanel, 
+  List, 
+  ListItem, 
+  ListIcon 
+} from '@chakra-ui/react'
+
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <Tabs mt="40px" p="20px" variant="enclosed" colorScheme="purple">
+      <TabList>
+        <Tab _selected={{ color: 'white', bg: 'purple.400' }}>Account Info</Tab>
+        <Tab _selected={{ color: 'white', bg: 'purple.400' }}>Task History</Tab>
+      </TabList>
+
+      <TabPanels py="10px">
+        <TabPanel>
+          <List spacing={4}>
+            <ListItem>
+              <ListIcon as={EmailIcon} />
+              Email: harry@gmail.com
+            </ListItem>
+            <ListItem>
+              <ListIcon as={ChatIcon} />
+              Connect with me so that we can collaborate on big projects.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={StarIcon} />
+              Was named Developer of the Month in the company I work in.
+            </ListItem>
+          </List>
+        </TabPanel>
+        <TabPanel>
+          <List spacing={4}>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="teal.400" />
+              Add header.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="teal.400" />
+              Style the login page.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={WarningIcon} color="red.400" />
+              Push the feature branch to GitHub.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="teal.400" />
+              Make the navbar reponsive.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={WarningIcon} color="red.400" />
+              Collaborate with the new Junior Developer.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="teal.400" />
+              Inform C.T.O of task completion.
+            </ListItem>
+          </List>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   )
 }
+
